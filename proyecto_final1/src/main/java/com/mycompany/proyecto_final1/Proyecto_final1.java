ef57227b12eebad323ac6b4e9e22a3a511ed5644
Josué mora <josuemora842@gmail.com>
2024-10-13 00:35:34 -0600

    agregando el proyecto

---
+++ proyecto_final1/src/main/java/com/mycompany/proyecto_final1/Proyecto_final1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_final1;
+
+/**
+ *
+ * @author chelo
+ */
+public class Proyecto_final1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+         System.out.println("Hello World!");
+          System.out.println("Hello World!");
+           System.out.println("Hello World!");
+          
+    }
+}
